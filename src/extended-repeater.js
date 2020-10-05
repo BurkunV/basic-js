cb8ac8c9c776faea2bcf5dbabb58b92ff6a05681
Burkun <electronik_est@mail.ru>
2020-10-05 13:41:41 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator === undefined) {
+		options.separator = '+';
+	}
+
+	if (options.addition === undefined) {
+		options.addition = '';
+	}
+
+	if (options.additionSeparator === undefined) {
+		options.additionSeparator = '|';
+	}
+
+	if (options.additionRepeatTimes === undefined) {
+		options.additionRepeatTimes = 1;
+	}
+
+	if (options.repeatTimes === undefined) {
+		options.repeatTimes = 1;
+	}
+
+	let res = '';
+	for (let i = 0; i < options.repeatTimes; i++) {
+		res += str;
+
+		for (let j = 0; j < options.additionRepeatTimes; j++) {
+			res += options.addition;
+
+			if (j !== options.additionRepeatTimes - 1) {
+				res += options.additionSeparator;;
+			}
+		}
+
+		if (i !== options.repeatTimes - 1) {
+			res += options.separator;
+		}
+
+	}
+	return res;
 };
   
